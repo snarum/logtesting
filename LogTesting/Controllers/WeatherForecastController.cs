fc5879f7bf83cfccc6092769bdec37fc4d27d800
Snorre Garmann <snorre@enso.no>
2019-12-04 10:58:45 +0100

    v1.0

---
+++ LogTesting/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LogTesting.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IValidatorService _vs;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IValidatorService vs)
+        {
+            _logger = logger;
+            _vs = vs;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _vs.Validate(1, 2, 3);
+
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ LogTesting/ValidatorService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogTesting
+{
+    public interface IValidatorService
+    {
+        bool Validate(int x, int y, int anwser);
+    }
+    public class ValidatorService:IValidatorService
+    {
+        private ILogger<ValidatorService> _logger;
+
+        public ValidatorService(ILogger<ValidatorService> logger)
+        {
+            _logger = logger;
+        }
+
+        public bool Validate(int x, int y, int answer)
+        {
+            _logger.LogInformation("tutu");
+            return x + y == answer;
+        }
+    }
+}
